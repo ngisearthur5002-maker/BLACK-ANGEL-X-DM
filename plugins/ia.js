259171e1f9aa103470daf22de69f495c86cf03b8
ngisearthur5002-maker <ngisearthur5002@gmail.com>
2026-06-10 22:01:33 +0200

    Delete ia.js

--- plugins/ia.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (sock, from, text) => {
-    if (text === "ai" || text === "gpt") {
-        await sock.sendMessage(from, {
-            text: "🤖 IA BLACK ANGEL:\nJe suis actif mais pas encore connecté à OpenAI."
-        });
-    }
-};
