451c873c2ce4046d04e354c3d6387123eeb543e0
ngisearthur5002-maker <ngisearthur5002@gmail.com>
2026-06-10 21:20:43 +0200

    Create setting.js

---
+++ config/setting.js
@@ -0,0 +1,9 @@
+module.exports = {
+  botName: "BLACK ANGEL X MD",
+  ownerNumber: "243XXXXXXXXX",
+  prefix: ".",
+  sessionName: "auth_info_baileys",
+  mode: "public", // public / private
+  autoRead: true,
+  autoTyping: true
+};
