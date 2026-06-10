0865d4ccab08b3a2648a8e6ffa193716db4154f1
ngisearthur5002-maker <ngisearthur5002@gmail.com>
2026-06-10 22:02:02 +0200

    Delete group.js

--- plugins/group.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (sock, from, text) => {
-    if (text === "tagall") {
-        await sock.sendMessage(from, {
-            text: "👥 Tag all (fonction à améliorer plus tard)"
-        });
-    }
-};
