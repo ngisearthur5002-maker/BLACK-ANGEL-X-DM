4b3152c78d7337701e4958902d531778dca6ee45
ngisearthur5002-maker <ngisearthur5002@gmail.com>
2026-06-10 22:03:03 +0200

    Delete download.js

--- plugins/download.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (sock, from, text) => {
-    if (text === "youtube") {
-        await sock.sendMessage(from, {
-            text: "📥 Downloader YouTube (à connecter API plus tard)"
-        });
-    }
-};
